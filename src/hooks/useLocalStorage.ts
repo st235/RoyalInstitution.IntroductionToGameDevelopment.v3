b191b3d02cdbd22f18e1d51baf5ceb384d7fc513
Alex Dadukin <alexanderdadukin@gmail.com>
2025-06-28 22:24:10 +0100

    Release 0.0.2

--- src/hooks/useLocalStorage.ts
+++
@@ -1,8 +0,0 @@
-import { SaveToLocalStorage, ReadFromLocalStorage, RemoveFromLocalStorage } from "../utils/LocalStorageUtil";
-
-export const useLocalStorage = (key: string) => {
-  const setItem = <T>(value: T): boolean => SaveToLocalStorage<T>(key, value);
-  const getItem = <T>(): T | undefined => ReadFromLocalStorage<T>(key);
-  const removeItem = (): boolean => RemoveFromLocalStorage(key);
-  return { getItem, setItem, removeItem };
-};
--- src/reducers/pages/pageGameAndLearnSlice.ts
+++
@@ -1,7 +0,0 @@
-import { pageCommonStateSliceFactory } from "./pageCommonSliceFactory";
-
-const pageGameAndLearnSlice = pageCommonStateSliceFactory("GameAndLearn");
-
-export { pageGameAndLearnSlice };
-export const { updateSandbox } = pageGameAndLearnSlice.actions;
-export default pageGameAndLearnSlice.reducer;
